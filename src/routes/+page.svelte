6d46e307944e3240a3c60e05c1b2e8e7f78130f9
Zack <zack@zacharymaril.com>
2022-09-14 21:50:41 -0400

    it worked

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { WebPlayback } from 'svelte-spotify-web-playback';
+    import CustomPlayer from '../player.svelte';
+    const client_id = '4e64555654894d41b0878d4efb7a4c15';
+    let wrapper; // Reference to our wrapper element.
+    let player;
+  </script>
+  
+  <WebPlayback 
+  {client_id} 
+  name="My Spotify Player" 
+  volume={0.5} 
+  bind:this={wrapper}
+>
+
+  <div slot="login">
+      <button on:click={() => wrapper.login()}>Login with Spotify</button>
+  </div>
+
+  <CustomPlayer slot="player" let:player let:state {player} {state} />
+</WebPlayback>
+
